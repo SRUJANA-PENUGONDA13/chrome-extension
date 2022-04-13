5afa30b9ba7b51c6bfee7a7b3d6e774481d0ce33
Srujana Penugonda <srujanapenugonda1318@outlook.com>
2022-04-13 15:41:26 +0530

    Refactor - Updated greetings logic

--- src/utils/greeting.js
+++ src/utils/greeting.js
@@ -1,10 +1,16 @@
 const getGreeting = (time) => {
   const userName = localStorage.getItem("user-name");
   let greeting = "";
+  const hours = parseInt(time.split(":")[0]);
+
   if (time.toString().includes("AM")) {
     greeting = "Good Morning " + userName;
   } else {
-    greeting = "Good Afternoon " + userName;
+    if (hours === 12 || (hours >= 1 && hours < 6)) {
+      greeting = "Good Afternoon " + userName;
+    } else {
+      greeting = "Good Evening " + userName;
+    }
   }
 
   return greeting;
